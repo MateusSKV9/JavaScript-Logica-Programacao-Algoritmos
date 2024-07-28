275b2fe7e7b01daad8956a3d15ec7d6218878451
Mateus Santos <mateussilvanzg@gmail.com>
2024-07-27 21:30:12 -0300

    Exercise completed

--- Capitulo 05/js/ex5_3.js
+++ Capitulo 05/js/ex5_3.js
@@ -1,5 +1,6 @@
 var carros = [];
 
+// ADICIONAR
 function adicionarCarro() {
     var inModelo = document.getElementById('inModelo');
     var inPreco = document.getElementById('inPreco');
@@ -12,16 +13,64 @@ function adicionarCarro() {
         inModelo.focus();
         return;
     }
+    
+    for(var i = 0; i<carros.length; i++) {
+        if(carros[i].modelo === modelo);
+        alert('Modelo já adicionado.');
+        return;
+    }
 
     carros.push({ modelo: modelo, preco: preco });
+
+    inModelo.value = '';
+    inPreco.value = ''; 
+    outLista.textContent = ''
+
+    listarCarros();
 }
 var btnAdicionar = document.getElementById('btnAdicionar');
 btnAdicionar.addEventListener('click', adicionarCarro);
 
+// LISTAR
 function listarCarros() {
-    var outLista = document.getElementById('outLista');
+    if(carros.length == 0) {
+        alert('Sem carros registrados. Adicione-os.');
+        inModelo.focus();
+        return;
+    }
+
+    var lista = ''
 
     for(var i = 0; i<carros.length; i++) {
-        outLista.textContent += carros[i].modelo + ' - R$: '+carros[i].preco;
+        lista += carros[i].modelo + ' - R$: '+carros[i].preco.toFixed(2)+'\n';
+    }
+
+    document.getElementById('outLista').textContent = lista;
+}
+var btnListar = document.getElementById('btnListar');
+btnListar.addEventListener('click', listarCarros);
+
+// FILTRAR
+function filtrarCarros() {
+    var maximo = Number(prompt('Qual o valor máximo que o cliente deseja pagar?'));
+
+    if(maximo==0 || isNaN(maximo)) {
+        return;
+    }
+
+    var lista = '';
+
+    for(var i = 0; i<carros.length; i++) {
+        if(carros[i].preco <= maximo) {
+            lista += carros[i].modelo + ' - R$: '+carros[i].preco.toFixed(2)+'\n';
+        }
+    }
+
+    if(lista == '') {
+        outLista.textContent = 'Não há carros com preço até R$ '+maximo.toFixed(2);
+    } else {
+        outLista.textContent = 'Carros até R$ '+maximo.toFixed(2)+'\n------------------------------\n'+lista;
     }
 }
+var btnFiltrar = document.getElementById('btnFiltrar');
+btnFiltrar.addEventListener('click', filtrarCarros);
