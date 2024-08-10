629cfda8ff35f585532ff5d658ec5f81edc94fa9
Mateus Santos <mateussilvanzg@gmail.com>
2024-08-10 13:20:01 -0300

    exercise completed

--- Capitulo 09/js/ex9_1.js
+++ Capitulo 09/js/ex9_1.js
@@ -10,7 +10,7 @@ function adicionarTarefa() {
     }
 
     var divQuadro = document.getElementById('divQuadro');
-    var h5 =document.createElement('h5');
+    var h5 = document.createElement('h5');
     var texto = document.createTextNode(tarefa);
     h5.appendChild(texto);
     divQuadro.appendChild(h5);
@@ -33,8 +33,8 @@ function selecionarTarefa() {
     }
 
     var aux = -1;
-    for(var i = 0; i<numH5.length; i++) {
-        if(h5[i].className = 'tarefaSelecionada') {
+    for(var i = 0; i<numH5; i++) {
+        if(h5[i].className == 'tarefaSelecionada') {
             h5[i].className = 'tarefaNormal';
             aux = i;
             break;
@@ -45,7 +45,7 @@ function selecionarTarefa() {
         aux = -1;
     }
 
-    h5[aux+1],className = 'tarefaSelecionada';
+    h5[aux+1].className = 'tarefaSelecionada';
 }
 var btnSelecionar = document.getElementById('btSelecionar');
 btnSelecionar.addEventListener('click', selecionarTarefa);
@@ -54,7 +54,7 @@ btnSelecionar.addEventListener('click', selecionarTarefa);
 // RETIRAR SELECIONADA
 function retirarSelecionada() {
     var divQuadro = document.getElementById('divQuadro');
-    var h5 = document.getElementById('h5');
+    var h5 = document.getElementsByTagName('h5');
     var numH5 = h5.length;
 
     var aux= -1;
@@ -75,5 +75,46 @@ function retirarSelecionada() {
         divQuadro.removeChild(h5[aux]);
     } 
 }
-var btnRetirar = document.getElementById('btnRetirar');
+var btnRetirar = document.getElementById('btRetirar');
 btnRetirar.addEventListener('click', retirarSelecionada);
+
+
+// GRAVAR TAREFAS
+function gravarTarefas() {
+    var h5 = document.getElementsByTagName('h5');
+    var numH5 = h5.length;
+
+    if(numH5==0) {
+        alert('Não há tarefas para serem salvas');
+        return;
+    }
+
+    var tarefas = '';
+
+    for(var i = 0; i<numH5; i++) {
+        tarefas += h5[i].textContent + ';';
+    }
+    localStorage.setItem('tarefasDia', tarefas.substr(0, tarefas.length-1));
+
+    if(localStorage.getItem('tarefasDia')) {
+        alert('Ok! Tarefas salvas');
+    }
+}
+var btnGravar = document.getElementById('btGravar');
+btnGravar.addEventListener('click', gravarTarefas);
+
+// RECUPERAR TEREFA
+function recuperarTarefas() {
+    if(localStorage.getItem('tarefasDia')) {
+        var tarefas = localStorage.getItem('tarefasDia').split(';');
+        var divQuadro = document.getElementById('divQuadro');
+
+        for(var i = 0; i<tarefas.length; i++) {
+            var h5 = document.createElement('h5');
+            var texto = document.createTextNode(tarefas[i]);
+            h5.appendChild(texto);
+            divQuadro.appendChild(h5);
+        }
+    }
+}
+recuperarTarefas();
